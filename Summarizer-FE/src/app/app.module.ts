2940bb1129ff67b83a6010c23f55d4177dc21021
Alex Pennini <alex.pennini@edu.unife.it>
2026-01-14 10:13:01 +0100

    Setup Summarizer

---
+++ Summarizer-FE/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
